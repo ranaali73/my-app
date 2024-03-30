6137fbd3f0d42192bd207c49ea33320265e20ec1
ranaali73 <ranaalizeeshan73@gmail.com>
2024-03-31 02:59:08 +0500

    router

--- src/app/components/blog/page.tsx
+++ src/app/components/blog/page.tsx
@@ -22,12 +22,12 @@ export default function Blog() {
             <div style={{ textAlign: 'center' }}>
                 <Link href="/" style={{ display: 'inline-block', fontSize: '20px', padding: '10px', backgroundColor: '#333', color: '#fff', textDecoration: 'none', width: '200px', marginTop: '30px', borderRadius: '5px' }}>Back to Home</Link>
                 <h1>What is useRouter</h1>
-                <p>Here, I have used useRouter, so what is useRouter. I'll explain in the following paragraph.</p>
+                <p>Here I have used useRouter so what is useRouter. I willl explain in the following paragraph.</p>
 
                 <h2>
                     Navigate Programmatically:</h2>
                 <p>
-                    Instead of using Link components, you might encounter situations where you need to implement logic or validations before navigating to the next page. For example, let's say you're on a blog page with a button labeled Add Post. You want to execute some logic, such as validating user input, before navigating to the next page after clicking this button. While the simple Link component can handle basic page-to-page navigation, it lacks the flexibility to execute custom logic or validations. This is where the useRouter hook comes into play. By using the useRouter hook, you can incorporate your custom logic and validations before initiating the navigation to the next page when a button is clicked. This allows for a more dynamic and interactive user experience in your Next.js application.
+                    Instead of using Link components you might encounter situations where you need to implement logic or validations before navigating to the next page. For example lets say you are on a blog page with a button labeled Add Post. You want to execute some logic such as validating user input before navigating to the next page after clicking this button. While the simple Link component can handle basic page to page navigation, it lacks the flexibility to execute custom logic or validations. This is where the useRouter hook comes into play. By using the useRouter hook you can incorporate your custom logic and validations before initiating the navigation to the next page when a button is clicked. This allows for a more dynamic and interactive user experience in your Next.js application.
                 </p>
             </div >
 
